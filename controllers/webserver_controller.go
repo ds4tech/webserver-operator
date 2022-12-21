0c0141bafa554cb8d9f661e3939118f0ec524a8f
Mateusz Szymczyk <mateusz.szymczyk@tuispain.com>
2022-12-21 16:14:53 +0100

    correct makers

--- controllers/webserver_controller.go
+++ controllers/webserver_controller.go
@@ -34,9 +34,9 @@ type WebserverReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=cache.example.com,resources=memcacheds,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=cache.example.com,resources=memcacheds/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=cache.example.com,resources=memcacheds/finalizers,verbs=update
+//+kubebuilder:rbac:groups=cache.ds4tech.com,resources=webservers,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=cache.ds4tech.com,resources=webservers/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=cache.ds4tech.com,resources=webservers/finalizers,verbs=update
 //+kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
